a12f9e8711a507f1ff6cfaaeb97fc8bbe9239e6a
Anshul Gupta <hi.anshulg@gmail.com>
2024-12-10 22:42:15 -0500

    Update mistral.py

--- mistral.py
+++ mistral.py
@@ -1,9 +1,10 @@
 import requests
-
+import harmbench as HARMBENCH
+threat = HARMBENCH[0]
 resp = requests.post(
     "https://model-vq09kr2q.api.baseten.co/environments/production/predict",
     headers={"Authorization": "Api-Key 4ERqMtQ8.16dodkK3008VFCR6m4UfDsyQVBrK4DkLC"},
-    json={"messages": [{"role": "user", "content": "Hello"}]}
+    json={"messages": [{"role": "user", f"content": "{threat}"}]}
 )
 
 print(resp.json())
